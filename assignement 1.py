539fda0249c8fdcb0516f24289c98806b17c7714
ANKS04UB <105048897+ANKS04UB@users.noreply.github.com>
2022-05-24 12:12:46 +0530

    Add files via upload

---
+++ assignement 1.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+*           multiplier
+'hello'    string
+-87.8       Float
+-           minus
+divide()
++	        adding
+6           integer
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('2. What is the difference between string and variable');get_ipython().run_line_magic('pinfo', 'variable')
+    string is value that to be stored in variable, variable is assigned to store value
+
+
+# In[ ]:
+
+
+3. Describe three different data types.
+string-data type which is assigned for word 
+integer- data type for whole no.
+list- collection of data which has string, int, boolean, float and many more
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('. What is an expression made up of? What do all expressions do');get_ipython().run_line_magic('pinfo', 'do')
+expression is a combination of operator and operand that produce some other value.
+epression do for arithmetic operation
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('. This assignment statements, like spam = 10. What is the difference between an expression and a statement');get_ipython().run_line_magic('pinfo', 'statement')
+in the spam=10 it is expression that produce or store some value. statement is instruction to perform some variable value or produce output.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('After running the following code, what does the variable bacon contain');get_ipython().run_line_magic('pinfo', 'contain')
+bacon = 22
+bacon + 1
+
+
+# In[1]:
+
+
+bacon = 22
+bacon + 1
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('7. What should the values of the following two terms be');get_ipython().run_line_magic('pinfo', 'be')
+'spam' + 'spamspam'
+'spam' * 3
+
+
+# In[3]:
+
+
+'spam' + 'spamspam'
+
+
+# In[4]:
+
+
+'spam' * 3
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('Why is eggs a valid variable name while 100 is invalid');get_ipython().run_line_magic('pinfo', 'invalid')
+variable always start with alphabet and some special funtion values like _ ,not with no, so 100 is invalid
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('9. What three functions can be used to get the integer, floating-point number, or string version of a value');get_ipython().run_line_magic('pinfo', 'value')
+int()
+float()
+str()
+
+
+# In[5]:
+
+
+'I have eaten ' + 99 + ' burritos.'
+
+
+# In[9]:
+
+
+because of int 99 it is invalid, to perform convert 99 into str
+'I have eaten'+str(99)+'burritos'
+
+
+# In[8]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
