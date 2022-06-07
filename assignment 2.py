aa95270bddc105bac2ad41e62501866dfc6d2819
ANKS04UB <105048897+ANKS04UB@users.noreply.github.com>
2022-06-07 15:45:02 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+ans1) two boolean data type
+True and False
+
+
+# In[ ]:
+
+
+Ans2) Three boolean operator is 
+AND, OR, NOT
+
+
+# In[ ]:
+
+
+Ans3)
+And Table                OR TABLE               NOT TABLE
+A   B    Q            A    B   Q               A  Q
+F   F     F           F     F   F              T  F
+F   T     F           F     T   T              F  T
+T   F     F           T     F   T
+T   T     T           T     T    T
+
+
+# In[ ]:
+
+
+Ans 4)(5 > 4) and (3 == 5) 5 greater than 4 and 3 equal to 5 ,False
+       not (5 > 4)         not 5 greater than 4              , False
+    (5 > 4) or (3 == 5)  5 greater than 4 or 3 equal to 5 , True
+    not ((5 > 4) or (3 == 5)) not 5 greater than 4 or 3 equal to 5 , False
+
+
+# In[ ]:
+
+
+Ans 5)  Less than ( < )
+        Less than or equal to ( <= )
+        Greater than ( > )
+        Greater than or equal to ( >= )
+        Equal to ( == )
+        Not equal to ( != )
+
+
+# In[ ]:
+
+
+Ans 6) assign operator incluse only = (it is used for variable assignment)
+        in Python equal to operator consist of == (checks whether the two given operands are equal or not )
+
+
+# In[ ]:
+
+
+Ans 7) Block 1 -spam = 0
+               if spam == 10:
+                  print('eggs')
+  Block 2  -if spam > 5:
+                print('bacon')
+
+        
+Block 3 -else:
+print('ham')
+print('spam')
+print('spam')
+
+
+# In[ ]:
+
+
+Ans 8)spam=0
+      if spam==1:
+        print("hello")
+    if spam==2:
+        print("Howdy")
+    else:
+        print("Greeting")
+        
+
+
+# In[ ]:
+
+
+Ans 9) Restart the kernal
+
+
+# In[ ]:
+
+
+Ans 10) The main difference between both the statements is that when break keyword comes, it terminates the execution of the current loop and passes the control over the next loop or main body, whereas when continue keyword is encountered, it skips the current iteration and executes the very next iteration in the loop. 
+
+
+# In[ ]:
+
+
+Ans 11) range(10) autogeneate no. from 0 to 9
+        range(0,10) starting no is 0 to ending no is 9
+        range (0,10,1) jump from 1 no ahead in between 0 to 9
+
+
+# In[ ]:
+
+
+#Ans 12) 
+        for i in range(11):
+           print(i, end=" ")
+        print()
+        
+#using while loop
+      i = 1
+       while(i<=10):
+            print(i,end=" ")
+            i += 1
+
+
+# In[ ]:
+
+
+Ans 13)import spam
+        bacon()
+
