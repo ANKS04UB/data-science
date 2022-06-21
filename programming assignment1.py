221a998cca29fcd4a329aab3da7424d2839ec308
ANKS04UB <105048897+ANKS04UB@users.noreply.github.com>
2022-06-21 15:59:20 +0530

    Add files via upload

---
+++ programming assignment1.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+# Ans1)
+print("Hello Python")
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+# Ans 2)
+a=int(input())
+b=int(input())
+addition=a+b
+division=a/b
+print("addition:",addition)
+print("division :",division)
+
+
+# In[4]:
+
+
+# Ans 3) Area of triangle
+height=int(input())
+base=int(input())
+triangle_area=(height*base)/2
+print(triangle_area)
+
+
+# In[6]:
+
+
+# Ans 4) Swap to variable
+a=56
+b=76
+a,b=b,a
+print(a)
+print(b)
+
+
+# In[12]:
+
+
+# Ans 5) Random Number
+import random
+n=random.randint(0,1000)
+print(n)
+
+
+# In[ ]:
+
+
+
+
