0772215ccea6cc09fd1c03c29a23c4dca78c5de1
ANKS04UB <105048897+ANKS04UB@users.noreply.github.com>
2022-06-21 15:16:43 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Ans 1)It helps to divide the large programs into small groups so that we can read the code, and debug the program faster and better.
+     ython Functions stop us from writing the same logic various times
+
+
+# In[ ]:
+
+
+Ans 2) When it is called 
+
+
+# In[ ]:
+
+
+Ans 3) def  keyword with block of codes created function.
+
+
+# In[ ]:
+
+
+Ans 4) A function is a block of code that does a particular operation and returns a result. It usually accepts inputs as parameters and returns a result. The parameters are not mandatory.
+       A function call is the code used to pass control to a function
+
+
+# In[ ]:
+
+
+Ans 5) one global Python scope per program execution
+       local scope function block per execution
+
+
+# In[ ]:
+
+
+Ans 6) A local variable retains its value until the next time the function is called A local variable becomes undefined after the function call completes The local variable can be used outside the function any time after the function call completes
+
+
+# In[ ]:
+
+
+Ans 7)A return statement is used to end the execution of the function call and “returns” the result (value of the expression following the return keyword) to the caller
+      yes, have a return value in an expression
+
+
+# In[ ]:
+
+
+Ans 8)The function always returns None if explicit return is not written
+
+
+# In[ ]:
+
+
+Ans 9) Create a variable outside of a function, and use it inside the function. ...
+Create a variable inside a function, with the same name as the global variable. ...
+If you use the global keyword, the variable belongs to the global scope
+
+
+# In[ ]:
+
+
+Ans 10) None is a data type of its own
+
+
+# In[ ]:
+
+
+Ans 11) That import statement imports a module named areallyourpetsnamederic
+
+
+# In[ ]:
+
+
+Ans 12) This function can be called with spam. bacon()
+
+
+# In[ ]:
+
+
+Ans 13) Python would process all code inside the try and except statement. When it encounters an error, the control is passed to the except block, skipping the code in between
+
+
+# In[ ]:
+
+
+Ans 14) The try block lets you test a block of code for errors. The except block lets you handle the error.
+
