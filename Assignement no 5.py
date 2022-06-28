cdf44510866feb1145a9c5b78ded70e3c0d3cd0b
ANKS04UB <105048897+ANKS04UB@users.noreply.github.com>
2022-06-28 13:46:40 +0530

    Add files via upload

---
+++ Assignement no 5.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+Ans 1)
+empty dictionert look like {}
+
+
+# In[5]:
+
+
+Ans 2)
+{foo:42}
+
+
+# In[6]:
+
+
+Ans 3)
+
+The items stored in a dictionary are unordered, while the items in a list are ordered.
+
+
+# In[18]:
+
+
+#Ans 4)
+spam={"bar":100}
+spam['foo']
+#You get a KeyError error.
+
+
+# In[11]:
+
+
+Ans 5)
+There is no difference. The in operator checks whether a value exists as a key in the dictionary.
+
+
+# In[ ]:
+
+
+Ans 6)
+'cat' in spam checks whether there is a 'cat' key in the dictionary, while 'cat' in spam.values() checks whether there is a value 'cat' for one of the keys in spam.
+
+
+# In[ ]:
+
+
+#Ans 7)
+spam.setdefault('color', 'black')
+
+
+# In[20]:
+
+
+# Ans 8)
+pprint.pprint()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
