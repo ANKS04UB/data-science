3321c687b51ae980494252aec1d79a844febe75d
ANKS04UB <105048897+ANKS04UB@users.noreply.github.com>
2022-07-06 15:47:10 +0530

    Add files via upload

---
+++ assigment no 6.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# ans 1)
+In Python strings, the backslash "\" is a special character, also called the "escape" character. It is used in representing certain whitespace characters: "\t" is a tab, "\n" is a newline, and "\r" is a carriage return
+
+
+# In[ ]:
+
+
+# ans 2)
+t" is a tab, "\n" is a newline
+
+
+# In[ ]:
+
+
+# ans 3)
+An escape character is a backslash \ followed by the character you want to insert.
+
+
+# In[ ]:
+
+
+# ans 4)
+here just because of this single quote we are getting an error. So we need to escape from this single quote.
+
+
+# In[ ]:
+
+
+# ans 5)
+use of end=" "
+
+
+# In[5]:
+
+
+# ans 6)
+s='Hello,word!'
+s[1]
+
+
+
+# In[6]:
+
+
+s[3:]
+
+
+# In[7]:
+
+
+s[0:5]
+
+
+# In[8]:
+
+
+s[:5]
+
+
+# In[9]:
+
+
+# ans 7)
+s='Hello'
+s.upper()
+
+
+# In[12]:
+
+
+s.upper().isupper()
+
+
+# In[13]:
+
+
+s.upper().lower()
+
+
+# In[14]:
+
+
+# ans 8)
+s='Remember,remember,the fifty of july.'.split()
+
+
+# In[15]:
+
+
+'-'.join('there can only one.'.split())
+
+
+# In[ ]:
+
+
+# ans 9)
+Python String ljust() function accepts the character for padding it to the input string. Then, it substitutes the character and does padding to the right of the input string.
+Python’s rjust() function accepts the character for padding it to the input string.
+
+Then, it substitutes the character and does padding to the left of the input string
+
+
+# In[ ]:
+
+
+# ans 10)
+The Python String strip() method is part of the built-in function available in python. The function will remove given characters from the start and end of the original string. This function is very helpful in removing the whitespaces at the start and end of the given string,
+
